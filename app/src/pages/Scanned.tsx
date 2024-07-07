f2e9a27d2cc35b9b33ec9af4522d34d9db1ab796
HectorC99 <hectorcampbell99@gmail.com>
2024-07-07 16:58:06 +0100

    update scanned page

--- app/src/pages/Scanned.tsx
+++ app/src/pages/Scanned.tsx
@@ -23,12 +23,16 @@ function Scanned() {
   }, [])
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100 pt-20">
+    <div className="flex flex-col items-center min-h-dvh bg-gray-100 pt-10 lg:pt-20">
       <Logo />
       <div className="flex flex-col mt-10 w-96">
         <p className="w-full mr-2 px-4 py-2 border rounded-full bg-white">{name}</p>
         <p className="w-full mr-2 px-4 py-2 border rounded-full bg-white">{message}</p>
       </div>
+      <p className="fixed mb-6 bottom-0 w-full text-center text-slate-500">
+        This was developed for fun by{' '}
+        <a className="underline text-green-600" href="https://www.linkedin.com/in/hector-campbell-9b671b145" target="_blank">Hector Campbell</a>
+      </p>
     </div>
   )
 }
