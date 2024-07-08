6ceec661cb4b1e18d3f91383efe714d999fabb9d
HectorC99 <hectorcampbell99@gmail.com>
2024-07-08 08:47:25 +0100

    update how it works popup

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -115,11 +115,10 @@ function Home({ }: Props) {
               className="w-full max-w-md rounded-xl bg-white/80 border p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
               <DialogTitle as="h3" className="text-base/7 font-medium text-black">
-                Payment successful
+                Display a message with a QR Code
               </DialogTitle>
               <p className="mt-2 text-sm/6 text-black/50">
-                Your payment has been successfully submitted. We've sent you an email with all of the details of your
-                order.
+                Put in your name and a message, and we'll generate a QR Code for you to share with others. When your scan the QR Code, they'll see your message displayed on this web page.
               </p>
               <div className="mt-4">
                 <Button
